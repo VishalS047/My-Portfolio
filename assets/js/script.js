55b4955e100f64aecd2f083c419610e490959ecf
Vishal Sharma <iamvishal047@gmail.com>
2021-06-08 00:21:32 +0530

    fixed some bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,44 @@ document.onreadystatechange = function () {
     AOS.init();
   }
 };
+
+
+$(document).on('click', '.mobile-nav-toggle', function(e) {
+  $('body').toggleClass('mobile-nav-active');
+  $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+});
+
+$(document).click(function(e) {
+  var container = $(".mobile-nav-toggle");
+  if (!container.is(e.target) && container.has(e.target).length === 0) {
+    if ($('body').hasClass('mobile-nav-active')) {
+      $('body').removeClass('mobile-nav-active');
+      $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+    }
+  }
+});
+
+// Skills section
+$('.skills-content').waypoint(function() {
+  $('.progress .progress-bar').each(function() {
+    $(this).css("width", $(this).attr("aria-valuenow") + '%');
+  });
+}, {
+  offset: '80%'
+});
+
+ // Back to top button
+ $(window).scroll(function() {
+  if ($(this).scrollTop() > 100) {
+    $('.totop').fadeIn('slow');
+  } else {
+    $('.totop').fadeOut('slow');
+  }
+});
+
+$('.totop').click(function() {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 1000, 'easeInOutExpo');
+  return false;
+});
